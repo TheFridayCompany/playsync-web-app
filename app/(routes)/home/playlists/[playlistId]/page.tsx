df9d25280fc4a5d25b9610c529f3ed5e9e2baca3
Rohan Dave <rohandewangdave@gmail.com>
2025-03-24 21:10:00 -0400

    conditional rendering UI

--- app/(routes)/home/playlists/[playlistId]/page.tsx
+++ app/(routes)/home/playlists/[playlistId]/page.tsx
@@ -11,9 +11,11 @@ import { useSelector } from "react-redux";
 const Sidebar = ({
   collaborators,
   onRemoveCollaborator,
+  shouldShowControls,
 }: {
   collaborators: User[];
   onRemoveCollaborator: (userId: string) => void;
+  shouldShowControls: boolean;
 }) => {
   return (
     <div className="w-1/4 bg-gray-800 text-white p-4">
@@ -23,12 +25,14 @@ const Sidebar = ({
           <li key={collaborator.id} className="mb-2">
             <div className="flex items-center justify-between">
               <span>{collaborator.username}</span>
-              <button
-                onClick={() => onRemoveCollaborator(collaborator.id)}
-                className="text-red-500 ml-2"
-              >
-                Remove
-              </button>
+              {shouldShowControls && (
+                <button
+                  onClick={() => onRemoveCollaborator(collaborator.id)}
+                  className="text-red-500 ml-2"
+                >
+                  Remove
+                </button>
+              )}
             </div>
           </li>
         ))}
@@ -95,6 +99,7 @@ const PlaylistPage = () => {
   const [collaborators, setCollaborators] = useState<User[]>([]);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const { friends } = useSelector((state: any) => state.social);
+  const { profile } = useSelector((state: any) => state.profile);
 
   useEffect(() => {
     if (playlistId) {
@@ -151,17 +156,22 @@ const PlaylistPage = () => {
     }
   };
 
+  const shouldShowControls =
+    (playlist && playlist?.userId === profile.id) ?? false;
+
   return (
     <div className="flex">
       {/* Playlist Content */}
       <div className="flex-1 p-4">
         <h1 className="text-2xl font-bold mb-4">Playlist ID: {playlistId}</h1>
-        <button
-          onClick={() => setIsModalOpen(true)}
-          className="mb-4 px-4 py-2 bg-blue-500 text-white rounded-md"
-        >
-          Add collaborator
-        </button>
+        {shouldShowControls && (
+          <button
+            onClick={() => setIsModalOpen(true)}
+            className="mb-4 px-4 py-2 bg-blue-500 text-white rounded-md"
+          >
+            Add collaborator
+          </button>
+        )}
         {playlist && (
           <>
             {playlist.songs.map((song) => (
@@ -179,10 +189,13 @@ const PlaylistPage = () => {
       </div>
 
       {/* Sidebar */}
-      <Sidebar
-        collaborators={collaborators}
-        onRemoveCollaborator={handleRemoveCollaborator}
-      />
+      {playlist && (
+        <Sidebar
+          collaborators={collaborators}
+          onRemoveCollaborator={handleRemoveCollaborator}
+          shouldShowControls={shouldShowControls}
+        />
+      )}
 
       {/* Modal to Add Collaborator */}
       <AddCollaboratorModal
@@ -196,3 +209,145 @@ const PlaylistPage = () => {
 };
 
 export default PlaylistPage;
+
+// "use client";
+
+// import Playlist from "@/app/features/playlist/domain/entities/playlist.entity";
+// import usePlaylists from "@/app/features/playlist/presentation/hooks/usePlaylists";
+// import { User } from "@/app/features/profile/domain/entities/user.entity";
+// import { SongCard } from "@/app/features/song/presentation/components/SongsList";
+// import { useParams } from "next/navigation";
+// import { useEffect, useState } from "react";
+// import { useSelector } from "react-redux";
+
+// const Sidebar = ({
+//   collaborators,
+//   onRemoveCollaborator,
+//   shouldShowControls,
+// }: {
+//   collaborators: User[];
+//   onRemoveCollaborator: (userId: string) => void;
+//   shouldShowControls: boolean;
+// }) => {
+//   return (
+//     <div className="w-1/4 bg-gray-800 text-white p-4">
+//       <h2 className="text-lg font-semibold mb-4">Collaborators</h2>
+//       <ul>
+//         {collaborators.map((collaborator) => (
+//           <li key={collaborator.id} className="mb-2">
+//             <div className="flex items-center justify-between">
+//               <span>{collaborator.username}</span>
+//               {shouldShowControls && (
+//                 <button
+//                   onClick={() => onRemoveCollaborator(collaborator.id)}
+//                   className="text-red-500 ml-2"
+//                 >
+//                   Remove
+//                 </button>
+//               )}
+//             </div>
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// };
+
+// const PlaylistPage = () => {
+//   const params = useParams();
+//   const playlistId = params?.playlistId as string;
+//   const {
+//     fetchPlaylist,
+//     removeSong,
+//     fetchCollaborators,
+//     addCollaborator,
+//     removeCollaborator,
+//   } = usePlaylists();
+//   const [playlist, setPlaylist] = useState<Playlist | null>(null);
+//   const [collaborators, setCollaborators] = useState<User[]>([]);
+//   const [isModalOpen, setIsModalOpen] = useState(false);
+//   const { friends } = useSelector((state: any) => state.social);
+//   const { profile } = useSelector((state: any) => state.profile);
+
+//   useEffect(() => {
+//     if (playlistId) {
+//       handleFetchCollaborators(playlistId);
+//       const p = fetchPlaylist(playlistId);
+//       if (p) setPlaylist(p);
+//     }
+//   }, [playlistId]);
+
+//   const handleFetchCollaborators = async (playlistId: string) => {
+//     try {
+//       const collaborators = await fetchCollaborators(playlistId);
+//       if (collaborators) setCollaborators(collaborators);
+//     } catch (e) {
+//       console.error(e);
+//     }
+//   };
+
+//   const handleRemoveSong = async (songId: string) => {
+//     try {
+//       if (playlist) {
+//         await removeSong(playlistId, songId);
+//         setPlaylist({
+//           ...playlist,
+//           songs: playlist.songs.filter((song) => song.id !== songId),
+//         });
+//       }
+//     } catch (e) {
+//       console.error(e);
+//     }
+//   };
+
+//   const handleRemoveCollaborator = async (userId: string) => {
+//     try {
+//       await removeCollaborator(playlistId, userId);
+//       setCollaborators((prevCollaborators) =>
+//         prevCollaborators.filter((collaborator) => collaborator.id !== userId)
+//       );
+//     } catch (e) {
+//       console.error(e);
+//     }
+//   };
+
+//   return (
+//     <div className="flex">
+//       {/* Playlist Content */}
+//       <div className="flex-1 p-4">
+//         <h1 className="text-2xl font-bold mb-4">Playlist ID: {playlistId}</h1>
+//         <button
+//           onClick={() => setIsModalOpen(true)}
+//           className="mb-4 px-4 py-2 bg-blue-500 text-white rounded-md"
+//         >
+//           Add collaborator
+//         </button>
+//         {playlist && (
+//           <>
+//             {playlist.songs.map((song) => (
+//               <SongCard key={song.id} song={song}>
+//                 <button
+//                   onClick={(_) => handleRemoveSong(song.id)}
+//                   className="text-red-500"
+//                 >
+//                   Remove song
+//                 </button>
+//               </SongCard>
+//             ))}
+//           </>
+//         )}
+//       </div>
+
+//       {/* Sidebar */}
+//       {playlist && (
+//         <Sidebar
+//           collaborators={collaborators}
+//           onRemoveCollaborator={handleRemoveCollaborator}
+//           shouldShowControls={playlist?.userId === profile.id}
+//         />
+//       )}
+//     </div>
+//   );
+// };
+
+// export default PlaylistPage;
